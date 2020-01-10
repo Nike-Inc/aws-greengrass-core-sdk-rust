9271bd3ccb538cdc540a678bab76d1d27463585d
Jack Wright <jack.wright@nike.com>
2020-01-10 15:53:57 -0800

    changed runtime initialization

--- src/runtime.rs
+++ src/runtime.rs
@@ -2,6 +2,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use crate::context::LambdaContext;
 use crate::error::GGError;
+use std::default::Default;
 
 pub enum RuntimeOption {
     Async,
@@ -15,18 +16,37 @@ impl RuntimeOption {
     }
 }
 
-// handler is just a place hodler for the time being
-pub fn start<F>(handler: Option<F>, option: RuntimeOption) -> Result<(), GGError>
-where
-    F: Fn(LambdaContext) -> (),
-{
-    unsafe {
-        // todo - support handlers
-        // this will probably involve creating a handler with a channel that then
-        // sends info to our handler function: https://doc.rust-lang.org/nomicon/ffi.html#asynchronous-callbacks
-        extern "C" fn no_op_handler(_: *const gg_lambda_context) {};
-        let start_res = gg_runtime_start(Some(no_op_handler), option.as_opt());
-        GGError::from_code(start_res)?;
+pub struct Runtime {
+    runtime_option: RuntimeOption
+}
+
+impl Default for Runtime {
+    fn default() -> Self {
+        Runtime {
+            runtime_option: RuntimeOption::Async
+        }
     }
-    Ok(())
+}
+
+impl Runtime {
+
+    pub fn start(self) -> Result<(), GGError> {
+        unsafe {
+            // todo - support handlers
+            // this will probably involve creating a handler with a channel that then
+            // sends info to our handler function: https://doc.rust-lang.org/nomicon/ffi.html#asynchronous-callbacks
+            extern "C" fn no_op_handler(_: *const gg_lambda_context) {};
+            let start_res = gg_runtime_start(Some(no_op_handler), self.runtime_option.as_opt());
+            GGError::from_code(start_res)?;
+        }
+        Ok(())
+    }
+
+    pub fn with_runtime_option(self, runtime_option: RuntimeOption) -> Self {
+        Runtime {
+            runtime_option,
+            ..self
+        }
+    }
+
 }

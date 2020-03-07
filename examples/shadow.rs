cadf59b1d14cff2097e5e7bfa2edc8cfcc300af4
Jack Wright <jack.wright@nike.com>
2020-03-06 17:59:10 -0800

    Allowing the type to be any serde deserializable type on get. Implemented update and delete.

--- examples/shadow.rs
+++ examples/shadow.rs
@@ -1,27 +1,52 @@
-use aws_greengrass_core_rust::{log as gglog, GGResult};
-use aws_greengrass_core_rust::init;
-use aws_greengrass_core_rust::shadow::{ShadowClient, ShadowThing};
+use aws_greengrass_core_rust::handler::{Handler, LambdaContext};
+use aws_greengrass_core_rust::log as gglog;
+use aws_greengrass_core_rust::runtime::Runtime;
+use aws_greengrass_core_rust::{Initializer, GGResult};
+use aws_greengrass_core_rust::shadow::ShadowClient;
+use serde_json::Value;
+
 use log::{error, info, LevelFilter};
 use std::{thread, time};
+
+const DEFAULT_THING_NAME: &'static str = "foo";
+
+struct ShadowHandler {
+    thing_name: String,
+}
+
+impl ShadowHandler {
+    pub fn new() -> Self {
+        let thing_name = std::env::var("THING_NAME")
+            .unwrap_or(DEFAULT_THING_NAME.to_owned());
+        ShadowHandler {
+            thing_name
+        }
+    }
+}
+
+impl Handler for ShadowHandler {
+    fn handle(&self, _ctx: LambdaContext) {
+        if let Err(e) = do_stuff_with_thing() {
+            error!("Error calling shadows api: {}", e);
+        }
+    }
+}
 
 fn main() {
     gglog::init_log(LevelFilter::Debug);
     info!("Starting shadow gg lambda");
 
-    if let Err(e) = do_stuff_with_thing() {
-        error!("Error: {}", e);
-        std::process::exit(1);
+    let runtime = Runtime::default().with_handler(Some(Box::new(ShadowHandler::new())));
+
+    if let Err(e) = Initializer::default().with_runtime(runtime).init() {
+        error!("green grass initialization error: {}", e)
     }
 }
 
 
 fn do_stuff_with_thing() -> GGResult<()> {
-    init()?;
-
-    let (thing, response) = ShadowClient::get_thing_shadow("foo")?;
-
+    let (thing, response) = ShadowClient::default().get_thing_shadow::<Value>("foo")?;
     info!("response: {:?}", response);
     info!("Shadow Thing: {:#?}", thing);
-
     Ok(())
 }
--- src/shadow.rs
+++ src/shadow.rs
@@ -8,29 +8,115 @@ use crate::bindings::*;
 use crate::request::{GGRequestResponse, read_response_data};
 use crate::GGResult;
 use crate::error::GGError;
-
-#[derive(Debug, Clone)]
-pub struct ShadowThing {
-    //todo -- change me
-    pub json: Value,
-}
+use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
+use std::default::Default;
 
 pub struct ShadowClient;
 
 impl ShadowClient {
 
-    pub fn get_thing_shadow(thing_name: &str) -> GGResult<(ShadowThing, GGRequestResponse)> {
-        let (bytes, response) = read_shadow_thing(thing_name)?;
-        let json: Value = serde_json::from_slice(&bytes).map_err(GGError::from)?;
-        let thing = ShadowThing {
-            json
-        };
-        Ok((thing,response))
+    /// Get thing shadow for thing name.
+    ///
+    /// # Arguments
+    ///
+    /// * `thing_name` - The name of the device for the thing shadow to get
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use serde_json::Value;
+    /// use aws_greengrass_core_rust::shadow::ShadowClient;
+    ///
+    /// if let Ok((json, res)) = ShadowClient::default().get_thing_shadow::<Value>("my_thing") {
+    ///     println!("Retrieved: {:?}", json);
+    /// }
+    /// ```
+    pub fn get_thing_shadow<'a, T: DeserializeOwned>(&self, thing_name: &str) -> GGResult<(T, GGRequestResponse)> {
+        let (bytes, response) = read_thing_shadow(thing_name)?;
+        let json: T = serde_json::from_slice(&bytes).map_err(GGError::from)?;
+        Ok((json,response))
+    }
+
+    /// Updates a shadow thing with the specified document.
+    ///
+    /// # Arguments
+    ///
+    /// * `thing_name` - The name of the device to update the shadow document
+    /// * `doc` - Json serializable content to update
+    pub fn update_thing_shadow<T: Serialize>(&self, thing_name: &str, doc: &T) -> GGResult<GGRequestResponse> {
+        let json_string = serde_json::to_string(doc).map_err(GGError::from)?;
+        unsafe {
+            let thing_name_c = CString::new(thing_name).map_err(GGError::from)?;
+            let json_string_c = CString::new(json_string).map_err(GGError::from)?;
+
+            let mut req: gg_request = ptr::null_mut();
+            let req_init = gg_request_init(&mut req);
+            GGError::from_code(req_init)?;
+
+            let mut res = gg_request_result {
+                request_status: gg_request_status_GG_REQUEST_SUCCESS,
+            };
+
+            let update_res = gg_update_thing_shadow(
+                req,
+                thing_name_c.as_ptr(),
+                json_string_c.as_ptr(),
+                &mut res,
+            );
+
+            let close_res = gg_request_close(req);
+            GGError::from_code(close_res)?;
+
+            GGRequestResponse::try_from(&res)
+        }
+    }
+
+    /// Deletes thing shadow for thing name.
+    ///
+    /// # Arguments
+    ///
+    /// * `thing_name` - The name of the device for the thing shadow to get
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use serde_json::Value;
+    /// use aws_greengrass_core_rust::shadow::ShadowClient;
+    ///
+    /// let res = ShadowClient::default().delete_thing_shadow("my_thing");
+    /// ```
+    pub fn delete_thing_shadow(&self, thing_name: &str) -> GGResult<GGRequestResponse> {
+        unsafe {
+            let mut req: gg_request = ptr::null_mut();
+            let req_init = gg_request_init(&mut req);
+            GGError::from_code(req_init)?;
+
+            let thing_name_c = CString::new(thing_name).map_err(GGError::from)?;
+
+            let mut res = gg_request_result {
+                request_status: gg_request_status_GG_REQUEST_SUCCESS,
+            };
+
+            let delete_res = gg_delete_thing_shadow(req, thing_name_c.as_ptr(), &mut res);
+            GGError::from_code(req_init)?;
+
+            let close_res = gg_request_close(req);
+            GGError::from_code(close_res)?;
+
+            GGRequestResponse::try_from(&res)
+        }
     }
 
 }
 
-fn read_shadow_thing(thing_name: &str) -> GGResult<(Vec<u8>, GGRequestResponse)> {
+impl Default for ShadowClient {
+    fn default() -> Self {
+        ShadowClient
+    }
+}
+
+fn read_thing_shadow(thing_name: &str) -> GGResult<(Vec<u8>, GGRequestResponse)> {
     unsafe {
         let mut req: gg_request = ptr::null_mut();
         let req_init = gg_request_init(&mut req);

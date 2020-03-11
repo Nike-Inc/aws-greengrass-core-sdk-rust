56947471b05ea1f767de37255df550080ae11a6c
Jack Wright <jack.wright@nike.com>
2020-03-11 12:51:47 -0700

    added a test binding functions and unit tested request::read_response

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,1 +1,250 @@
+#[cfg(not(test))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+pub use self::test::*;
+
+/// Provides stubbed testing versions of methods, etc that match greengrasssdk.h
+/// Useful for internal testing.
+#[cfg(test)]
+pub mod test {
+    use std::thread_local;
+    use std::cell::RefCell;
+    use std::os::raw::c_void;
+
+    #[derive(Debug, Copy, Clone)]
+    pub struct _gg_request {
+        _unused: [u8; 0],
+    }
+    pub type gg_request = *mut _gg_request;
+    pub const gg_request_status_GG_REQUEST_SUCCESS: gg_request_status = 0;
+    pub const gg_request_status_GG_REQUEST_HANDLED: gg_request_status = 1;
+    pub const gg_request_status_GG_REQUEST_UNHANDLED: gg_request_status = 2;
+    pub const gg_request_status_GG_REQUEST_UNKNOWN: gg_request_status = 3;
+    pub const gg_request_status_GG_REQUEST_AGAIN: gg_request_status = 4;
+    pub const gg_request_status_GG_REQUEST_RESERVED_MAX: gg_request_status = 5;
+    pub const gg_request_status_GG_REQUEST_RESERVED_PAD: gg_request_status = 2147483647;
+    pub type gg_request_status = u32;
+    #[repr(C)]
+    #[derive(Debug, Copy, Clone)]
+    pub struct gg_request_result {
+        pub request_status: gg_request_status,
+    }
+
+    pub const gg_error_GGE_SUCCESS: gg_error = 0;
+    pub const gg_error_GGE_OUT_OF_MEMORY: gg_error = 1;
+    pub const gg_error_GGE_INVALID_PARAMETER: gg_error = 2;
+    pub const gg_error_GGE_INVALID_STATE: gg_error = 3;
+    pub const gg_error_GGE_INTERNAL_FAILURE: gg_error = 4;
+    pub const gg_error_GGE_TERMINATE: gg_error = 5;
+    pub const gg_error_GGE_RESERVED_MAX: gg_error = 6;
+    pub const gg_error_GGE_RESERVED_PAD: gg_error = 2147483647;
+    pub type gg_error = u32;
+
+    pub const gg_queue_full_policy_options_GG_QUEUE_FULL_POLICY_BEST_EFFORT: gg_queue_full_policy_options = 0;
+    pub const gg_queue_full_policy_options_GG_QUEUE_FULL_POLICY_ALL_OR_ERROR: gg_queue_full_policy_options = 1;
+    pub const gg_queue_full_policy_options_GG_QUEUE_FULL_POLICY_RESERVED_MAX: gg_queue_full_policy_options = 2;
+    pub const gg_queue_full_policy_options_GG_QUEUE_FULL_POLICY_RESERVED_PAD: gg_queue_full_policy_options = 2147483647;
+
+    pub type gg_queue_full_policy_options = u32;
+
+    #[derive(Debug, Copy, Clone)]
+    pub struct _gg_publish_options {
+        _unused: [u8; 0],
+    }
+
+    pub type gg_publish_options = *mut _gg_publish_options;
+
+    pub const gg_log_level_GG_LOG_RESERVED_NOTSET: gg_log_level = 0;
+    pub const gg_log_level_GG_LOG_DEBUG: gg_log_level = 1;
+    pub const gg_log_level_GG_LOG_INFO: gg_log_level = 2;
+    pub const gg_log_level_GG_LOG_WARN: gg_log_level = 3;
+    pub const gg_log_level_GG_LOG_ERROR: gg_log_level = 4;
+    pub const gg_log_level_GG_LOG_FATAL: gg_log_level = 5;
+    pub const gg_log_level_GG_LOG_RESERVED_MAX: gg_log_level = 6;
+    pub const gg_log_level_GG_LOG_RESERVED_PAD: gg_log_level = 2147483647;
+
+    pub type gg_log_level = u32;
+
+
+    pub fn gg_global_init(opt: u32) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_log(level: gg_log_level, format: *const ::std::os::raw::c_char) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_request_init(ggreq: *mut gg_request) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_request_close(ggreq: gg_request) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+
+    thread_local! {
+        pub static GG_REQUEST_READ_BUFFER: RefCell<Vec<u8>> = RefCell::new(vec![]);
+    }
+
+    pub fn gg_request_read(
+        ggreq: gg_request,
+        buffer: *mut ::std::os::raw::c_void,
+        buffer_size: usize,
+        amount_read: *mut usize,
+    ) -> gg_error {
+        unsafe {
+            GG_REQUEST_READ_BUFFER.with(|b| {
+                let mut borrowed = b.borrow().clone();
+                // If the vector is empty, don't do anything and notify that we read zero bytes
+                if borrowed.is_empty() {
+                    amount_read.write(0);
+                } else {
+                    // Find the index to split the array off at
+                    let index = if buffer_size > borrowed.len() {
+                        borrowed.len()
+                    } else {
+                        buffer_size
+                    };
+                    // borrowed will now contain everything up to index
+                    let next = borrowed.split_off(index);
+                    println!("gg_request_read: writing buffer: {:?}", borrowed);
+                    buffer.copy_from_nonoverlapping(borrowed.as_ptr() as *const c_void, borrowed.len());
+                    amount_read.write(borrowed.len());
+                    // replace the refcell with the rest of the vec
+                    b.replace(next);
+                }
+            });
+            gg_error_GGE_SUCCESS
+        }
+    }
+
+    #[derive(Debug, Copy, Clone)]
+    pub struct gg_lambda_context {
+        pub function_arn: *const ::std::os::raw::c_char,
+        pub client_context: *const ::std::os::raw::c_char,
+    }
+    pub type gg_lambda_handler = ::std::option::Option<unsafe fn(cxt: *const gg_lambda_context)>;
+
+    pub fn gg_runtime_start(handler: gg_lambda_handler, opt: u32) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_lambda_handler_read(
+        buffer: *mut ::std::os::raw::c_void,
+        buffer_size: usize,
+        amount_read: *mut usize,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_lambda_handler_write_response(
+        response: *const ::std::os::raw::c_void,
+        response_size: usize,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_lambda_handler_write_error(error_message: *const ::std::os::raw::c_char) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_get_secret_value(
+        ggreq: gg_request,
+        secret_id: *const ::std::os::raw::c_char,
+        version_id: *const ::std::os::raw::c_char,
+        version_stage: *const ::std::os::raw::c_char,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub const gg_invoke_type_GG_INVOKE_EVENT: gg_invoke_type = 0;
+    pub const gg_invoke_type_GG_INVOKE_REQUEST_RESPONSE: gg_invoke_type = 1;
+    pub const gg_invoke_type_GG_INVOKE_RESERVED_MAX: gg_invoke_type = 2;
+    pub const gg_invoke_type_GG_INVOKE_RESERVED_PAD: gg_invoke_type = 2147483647;
+    pub type gg_invoke_type = u32;
+    pub const gg_runtime_opt_GG_RT_OPT_ASYNC: gg_runtime_opt = 1;
+    pub const gg_runtime_opt_GG_RT_OPT_RESERVED_PAD: gg_runtime_opt = 2147483647;
+    pub type gg_runtime_opt = u32;
+
+    #[derive(Debug, Copy, Clone)]
+    pub struct gg_invoke_options {
+        pub function_arn: *const ::std::os::raw::c_char,
+        pub customer_context: *const ::std::os::raw::c_char,
+        pub qualifier: *const ::std::os::raw::c_char,
+        pub type_: gg_invoke_type,
+        pub payload: *const ::std::os::raw::c_void,
+        pub payload_size: usize,
+    }
+
+    pub fn gg_invoke(
+        ggreq: gg_request,
+        opts: *const gg_invoke_options,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_publish_options_init(opts: *mut gg_publish_options) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_publish_options_free(opts: gg_publish_options) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_publish_options_set_queue_full_policy(
+        opts: gg_publish_options,
+        policy: gg_queue_full_policy_options,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_publish_with_options(
+        ggreq: gg_request,
+        topic: *const ::std::os::raw::c_char,
+        payload: *const ::std::os::raw::c_void,
+        payload_size: usize,
+        opts: gg_publish_options,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_publish(
+        ggreq: gg_request,
+        topic: *const ::std::os::raw::c_char,
+        payload: *const ::std::os::raw::c_void,
+        payload_size: usize,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_get_thing_shadow(
+        ggreq: gg_request,
+        thing_name: *const ::std::os::raw::c_char,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_update_thing_shadow(
+        ggreq: gg_request,
+        thing_name: *const ::std::os::raw::c_char,
+        update_payload: *const ::std::os::raw::c_char,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+
+    pub fn gg_delete_thing_shadow(
+        ggreq: gg_request,
+        thing_name: *const ::std::os::raw::c_char,
+        result: *mut gg_request_result,
+    ) -> gg_error {
+        gg_error_GGE_SUCCESS
+    }
+}
--- src/request.rs
+++ src/request.rs
@@ -127,3 +127,46 @@ pub(crate) fn read_response_data(req_to_read: gg_request) -> Result<Vec<u8>, GGE
 
     Ok(bytes)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::ptr;
+    use crate::bindings::*;
+    use std::borrow::BorrowMut;
+
+    const READ_DATA: &[u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas maecenas pharetra. Ornare massa eget egestas purus viverra accumsan in nisl nisi. Dolor morbi non arcu risus. Vehicula ipsum a arcu cursus vitae. Luctus accumsan tortor posuere ac ut consequat semper viverra. At tempor commodo ullamcorper a lacus vestibulum sed. Dui ut ornare lectus sit amet. Tristique magna sit amet purus gravida quis blandit turpis. Duis at consectetur lorem donec. Amet cursus sit amet dictum sit. Lacus viverra vitae congue eu consequat ac felis donec et.
+
+Suscipit tellus mauris a diam. Odio tempor orci dapibus ultrices in. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Non diam phasellus vestibulum lorem sed risus. Amet justo donec enim diam vulputate ut pharetra sit amet. Est ullamcorper eget nulla facilisi etiam. Ut etiam sit amet nisl purus in mollis nunc. Vel eros donec ac odio tempor. Nascetur ridiculus mus mauris vitae ultricies.
+
+Purus sit amet volutpat consequat mauris nunc. Odio ut enim blandit volutpat. Etiam tempor orci eu lobortis elementum. Praesent tristique magna sit amet purus gravida. Interdum velit laoreet id donec ultrices tincidunt arcu non sodales. Sed pulvinar proin gravida hendrerit lectus. Lacus laoreet non curabitur gravida arcu. Turpis cursus in hac habitasse platea dictumst quisque. Tempor orci eu lobortis elementum nibh. Pellentesque massa placerat duis ultricies lacus sed turpis. Ut porttitor leo a diam sollicitudin tempor id eu nisl. Justo laoreet sit amet cursus. Ultrices in iaculis nunc sed augue lacus viverra vitae. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Sem nulla pharetra diam sit amet nisl. Suspendisse potenti nullam ac tortor vitae purus faucibus. Odio morbi quis commodo odio aenean. Justo nec ultrices dui sapien eget mi.
+
+Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Orci sagittis eu volutpat odio facilisis mauris sit amet. Nunc non blandit massa enim. Dui ut ornare lectus sit amet est placerat in egestas. Risus sed vulputate odio ut enim blandit volutpat. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Dolor magna eget est lorem ipsum. Velit sed ullamcorper morbi tincidunt ornare massa eget. Amet commodo nulla facilisi nullam vehicula ipsum. Velit dignissim sodales ut eu sem. Sed id semper risus in hendrerit gravida rutrum. Sit amet porttitor eget dolor morbi. Blandit turpis cursus in hac. Scelerisque felis imperdiet proin fermentum leo.
+
+Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Massa massa ultricies mi quis hendrerit. Sit amet facilisis magna etiam. Duis convallis convallis tellus id interdum velit laoreet id donec. Neque laoreet suspendisse interdum consectetur libero. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Amet volutpat consequat mauris nunc. Erat nam at lectus urna duis convallis convallis tellus. Consectetur a erat nam at lectus urna. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Volutpat blandit aliquam etiam erat. Semper quis lectus nulla at volutpat. Orci a scelerisque purus semper eget. Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum.
+
+Ultrices mi tempus imperdiet nulla. Purus in massa tempor nec feugiat nisl pretium fusce id. Praesent tristique magna sit amet purus. Facilisis volutpat est velit egestas dui. Sed egestas egestas fringilla phasellus faucibus scelerisque. Convallis a cras semper auctor. Viverra accumsan in nisl nisi. Aliquet nec ullamcorper sit amet risus. Massa sed elementum tempus egestas sed sed risus pretium. Tortor consequat id porta nibh. In tellus integer feugiat scelerisque varius morbi enim nunc. Adipiscing commodo elit at imperdiet dui accumsan. Tincidunt id aliquet risus feugiat in ante metus. Interdum varius sit amet mattis. Sit amet massa vitae tortor condimentum. Purus non enim praesent elementum. Vestibulum sed arcu non odio euismod lacinia at quis risus. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet.
+
+Vestibulum rhoncus est pellentesque elit. Feugiat nisl pretium fusce id velit ut tortor pretium. Tortor consequat id porta nibh venenatis cras sed felis eget. Velit scelerisque in dictum non consectetur a. Hendrerit gravida rutrum quisque non. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Tellus rutrum tellus pellentesque eu tincidunt. Sed arcu non odio euismod lacinia at quis. Netus et malesuada fames ac turpis egestas integer eget. Vitae justo eget magna fermentum iaculis eu non. Tincidunt praesent semper feugiat nibh sed pulvinar proin. Sodales ut eu sem integer vitae justo. Enim blandit volutpat maecenas volutpat blandit aliquam. Non odio euismod lacinia at quis risus sed. Mollis nunc sed id semper. Non enim praesent elementum facilisis leo vel fringilla. Viverra vitae congue eu consequat ac felis donec. Placerat orci nulla pellentesque dignissim. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Sed turpis tincidunt id aliquet.
+
+Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Lacus suspendisse faucibus interdum posuere lorem. Mauris cursus mattis molestie a iaculis at erat. Blandit massa enim nec dui nunc. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Adipiscing tristique risus nec feugiat in. Egestas sed sed risus pretium quam. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Ac placerat vestibulum lectus mauris ultrices. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Etiam erat velit scelerisque in dictum. At erat pellentesque adipiscing commodo. Mollis aliquam ut porttitor leo a diam sollicitudin tempor. Habitant morbi tristique senectus et netus et malesuada fames. Cras sed felis eget velit aliquet sagittis id consectetur purus. Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing. Risus nec feugiat in fermentum posuere urna nec.
+
+Nibh mauris cursus mattis molestie a iaculis. Diam ut venenatis tellus in. Nisl tincidunt eget nullam non nisi est sit. Adipiscing commodo elit at imperdiet dui accumsan sit amet. Tristique senectus et netus et. Vulputate ut pharetra sit amet aliquam. Non consectetur a erat nam at lectus urna duis. Aliquet sagittis id consectetur purus ut faucibus. Eget felis eget nunc lobortis mattis aliquam faucibus. Nisl tincidunt eget nullam non nisi. Nunc eget lorem dolor sed viverra ipsum nunc. Suspendisse faucibus interdum posuere lorem. Nisl condimentum id venenatis a condimentum. Malesuada pellentesque elit eget gravida cum sociis. Porta lorem mollis aliquam ut porttitor leo a diam. Maecenas volutpat blandit aliquam etiam.
+
+Parturient montes nascetur ridiculus mus mauris vitae ultricies. Suspendisse sed nisi lacus sed viverra. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Gravida in fermentum et sollicitudin. Sem et tortor consequat id porta nibh venenatis. Volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Amet cursus sit amet dictum sit amet justo donec enim. Nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Fermentum leo vel orci porta non pulvinar neque laoreet. Nunc sed id semper risus in hendrerit. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Tincidunt vitae semper quis lectus nulla at volutpat. Vel facilisis volutpat est velit egestas dui id ornare arcu. Vivamus arcu felis bibendum ut tristique et egestas quis. Sed vulputate odio ut enim blandit volutpat. Vel pharetra vel turpis nunc. Orci dapibus ultrices in iaculis nunc sed augue lacus. Vitae tempus quam pellentesque nec nam aliquam sem et tortor. Eget lorem dolor sed viverra ipsum. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada.";
+
+    #[test]
+    fn test_read_response_data() {
+        unsafe {
+            GG_REQUEST_READ_BUFFER.with( |buffer| buffer.replace(READ_DATA.to_owned()));
+            let mut req: gg_request = ptr::null_mut();
+            let result = gg_request_init(&mut req);
+            assert_eq!(result, gg_error_GGE_SUCCESS);
+
+            let result = read_response_data(req).unwrap();
+            assert!(!result.is_empty());
+            assert_eq!(result, READ_DATA);
+        }
+    }
+
+}

373282376900cf6b4f80c447b0f97452bff97a11
Jack Wright <jack.wright@nike.com>
2020-03-25 17:13:28 -0700

    removing unused docker artifacts

--- dockerbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker build -t aws-greengrass-core-sdk-rust-builder .
-docker run --rm -it \
-  -v "$(pwd)":/home/rust/src \
-  -v "$(pwd)/.dockercache/cargo-git":/home/rust/.cargo/git \
-  -v "$(pwd)/.dockercache/cargo-registry":/home/rust/.cargo/registry \
-  -v "$(pwd)/target/docker":/home/rust/src/target \
-  aws-greengrass-core-sdk-rust-builder $@
